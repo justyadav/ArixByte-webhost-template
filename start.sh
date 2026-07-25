010282fbf60527ed0f728d02696a60dff67718de
justyadav <yadavboymanish5656@gmail.com>
2026-07-25 20:49:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,49 +6,14 @@ echo "======================================"
 echo "     ArixByte Web Hosting"
 echo "======================================"
 
+echo "⏳ Preparing folders..."
 
-# Create writable folders
-
-mkdir -p /home/container/logs/nginx
-mkdir -p /home/container/logs/php
-mkdir -p /home/container/tmp/nginx/client_body
-mkdir -p /home/container/tmp/nginx/proxy
-mkdir -p /home/container/tmp/nginx/fastcgi
+mkdir -p /home/container/logs
+mkdir -p /home/container/tmp
 
 
-
-echo "⏳ Cleaning temporary files..."
-
-rm -rf /tmp/*
+echo "⏳ Starting Web Services..."
 
 
-
-# PHP-FPM
-
-echo "⏳ Starting PHP-FPM..."
-
-
-php-fpm84 \
--g /home/container/logs/php/php-fpm.pid \
--y /home/container/php-fpm/php-fpm.conf \
--D
-
-
-sleep 2
-
-
-echo "✅ PHP-FPM started successfully"
-
-
-
-# Nginx
-
-echo "⏳ Starting Nginx..."
-
-
-nginx \
--p /home/container/nginx \
--c /home/container/nginx/nginx.conf \
--g "daemon off;"
-
-
+# Use container default startup
+/usr/local/bin/start.sh
