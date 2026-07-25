6a48c10a86ab10d740edfb617d00557dd5f932af
justyadav <yadavboymanish5656@gmail.com>
2026-07-25 20:58:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,26 +9,32 @@ echo "======================================"
 echo "⏳ Preparing folders..."
 
 mkdir -p /home/container/logs
+mkdir -p /home/container/logs/nginx
+mkdir -p /home/container/logs/php
 mkdir -p /home/container/tmp
+mkdir -p /home/container/tmp/nginx/client_body
+mkdir -p /home/container/tmp/nginx/proxy
+mkdir -p /home/container/tmp/nginx/fastcgi
+
+
+echo "⏳ Configuring PHP-FPM..."
+
+cat > /home/container/php-fpm.conf <<EOF
+[global]
+
+error_log = /home/container/logs/php/php-fpm.log
+
+daemonize = no
+
+include=/etc/php84/php-fpm.d/*.conf
+EOF
 
 
 echo "⏳ Starting PHP-FPM..."
 
-# Find PHP-FPM automatically
-
-PHPFPM=$(find /usr -name "php-fpm*" -type f 2>/dev/null | head -n 1)
-
-
-if [ -z "$PHPFPM" ]; then
-    echo "❌ PHP-FPM binary not found"
-    exit 1
-fi
-
-
-echo "Using PHP-FPM: $PHPFPM"
-
-$PHPFPM -D
-
+php-fpm8 \
+-y /home/container/php-fpm.conf \
+-D
 
 
 sleep 2
@@ -37,10 +43,51 @@ sleep 2
 echo "✅ PHP-FPM started successfully"
 
 
+echo "⏳ Configuring Nginx..."
+
+
+cat > /home/container/nginx.conf <<EOF
+
+worker_processes auto;
+
+error_log /home/container/logs/nginx/error.log;
+
+pid /home/container/tmp/nginx.pid;
+
+
+events {
+    worker_connections 1024;
+}
+
+
+http {
+
+    include /etc/nginx/mime.types;
+
+    default_type application/octet-stream;
+
+
+    access_log /home/container/logs/nginx/access.log;
+
+
+    client_body_temp_path /home/container/tmp/nginx/client_body;
+
+    proxy_temp_path /home/container/tmp/nginx/proxy;
+
+    fastcgi_temp_path /home/container/tmp/nginx/fastcgi;
+
+
+    include /etc/nginx/http.d/*.conf;
+
+}
+
+EOF
+
+
+
 echo "⏳ Starting Nginx..."
 
 
 nginx \
--g "error_log /home/container/logs/nginx-error.log;" \
--g "pid /home/container/tmp/nginx.pid;" \
+-c /home/container/nginx.conf \
 -g "daemon off;"
